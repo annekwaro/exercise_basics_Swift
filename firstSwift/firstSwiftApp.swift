98f5e96ef188cb73536d5c8d0966fd0ab0ba858d
anne kwaro <anne>
2023-08-31 17:47:41 +0200

    Initial Commit

---
+++ firstSwift/firstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftApp.swift
+//  firstSwift
+//
+//  Created by Anne Kwaro on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
